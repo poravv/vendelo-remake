881139a97118cdf1a49598a031c29f2a85d0e69b
explwi <andres.vera@tigo.net.py>
2024-08-14 17:08:00 -0400

    Update git

--- src/servicios/detinventario.js
+++ src/servicios/detinventario.js
@@ -4,210 +4,226 @@ const jwt = require("jsonwebtoken");
 const det_inventario = require("../model/model_detinventario")
 const inventario = require("../model/model_inventario")
 const database = require('../database')
-const verificaToken = require('../middleware/token_extractor');
+const { keycloak } = require('../middleware/keycloak_validate');
 const { QueryTypes } = require('sequelize');
 let fechaActual = new Date();
 require("dotenv").config()
 
 
-routes.get('/getsql/', verificaToken, async (req, res) => {
-    try {
+routes.get('/getsql/', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
 
-        jwt.verify(req.token, process.env.CLAVESECRETA, async (error, authData) => {
-
-            if (error) {
-                res.json({ estado: "error", mensaje: error, })
-            } else {
-                const inv_analisis = await database.query(`select * from vw_det_inventario `, { type: QueryTypes.SELECT })
-                res.json({
-                    estado: "successfully",
-                    body: inv_analisis
-                })
-            }
-        })
-    } catch (error) {
-        res.json({ estado: "error", mensaje: error, })
-    }
+    await database.query(`select * from vw_det_inventario `,
+        { type: QueryTypes.SELECT }).then((response) => {
+            res.json({
+                mensaje: "successfully",
+                authData: authData,
+                body: response
+            });
+        }).catch(error => {
+            res.json({
+                mensaje: "error",
+                error: error,
+                detmensaje: `Error en el servidor, ${error}`
+            });
+        });
 })
 
-routes.get('/getinvdetsuc/:idinventario', verificaToken, async (req, res) => {
-    try {
-        jwt.verify(req.token, process.env.CLAVESECRETA, async (error, authData) => {
+routes.get('/getinvdetsuc/:idinventario', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
 
-            if (error) {
-                res.json({ estado: "error", mensaje: error, })
-            } else {
-                const idsucursal = authData?.rsusuario?.idsucursal;
-                const inv_analisis = await database.query(`select * from vw_det_inventario where idsucursal=${idsucursal} and idinventario=${req.params.idinventario}`, { type: QueryTypes.SELECT })
-                res.json({
-                    estado: "successfully",
-                    body: inv_analisis
-                })
-            }
-        })
-    } catch (error) {
-        res.json({ estado: "error", mensaje: error, })
-    }
+    const idsucursal = authData?.rsusuario?.idsucursal;
+    await database.query(`select * from vw_det_inventario where idsucursal=${idsucursal} and idinventario=${req.params.idinventario}`, { type: QueryTypes.SELECT })
+        .then((response) => {
+            res.json({
+                mensaje: "successfully",
+                authData: authData,
+                body: response
+            });
+        }).catch(error => {
+            res.json({
+                mensaje: "error",
+                error: error,
+                detmensaje: `Error en el servidor, ${error}`
+            });
+        });
 })
 
-routes.get('/get/', verificaToken, async (req, res) => {
-    try {
-        const det_inventarios = await det_inventario.findAll({});
+routes.get('/get/', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
 
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ estado: "error", mensaje: error })
-            } else {
-                res.json({
-                    estado: "successfully",
-                    body: det_inventarios
-                })
-            }
-        })
-    } catch (error) {
-        console.log(error)
-        res.json({ estado: "error", mensaje: error })
-    }
+    await det_inventario.findAll({}).then((response) => {
+        res.json({
+            mensaje: "successfully",
+            authData: authData,
+            body: response
+        });
+    }).catch(error => {
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
+    });
 });
 
-routes.get('/get/:idinventario', verificaToken, async (req, res) => {
+routes.get('/get/:idinventario', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
     try {
-
         if (req.params.idinventario) {
             //const det_inventarios = await det_inventario.findByPk(req.params.idinventario);
             const query = `select * from det_inventario where idinventario = ${req.params.idinventario} and estado ='AC'`;
-            const det_inventarios = await database.query(query,
+            await database.query(query,
                 {
                     model: det_inventario,
                     mapToModel: true // pass true here if you have any mapped fields
-                });
-
-            //console.log(det_inventarios);
-
-            jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-                if (error) {
-                    res.json({ estado: "error", mensaje: error })
-                } else {
+                }).then((response) => {
                     res.json({
-                        estado: "successfully",
-                        body: det_inventarios
-                    })
-                }
-            })
+                        mensaje: "successfully",
+                        authData: authData,
+                        body: response
+                    });
+                }).catch(error => {
+                    res.json({
+                        mensaje: "error",
+                        error: error,
+                        detmensaje: `Error en el servidor, ${error}`
+                    });
+                });
         } else {
-            res.send("Error idinventario null")
+            res.json({
+                mensaje: "error",
+                error: error,
+                detmensaje: `Error idinventario null`
+            });
         }
-
     } catch (error) {
-        res.json({ estado: "error", mensaje: error })
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
     }
 })
 
-routes.post('/post/', verificaToken, async (req, res) => {
+routes.post('/post/', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
     const t = await database.transaction();
     try {
-        jwt.verify(req.token, process.env.CLAVESECRETA, async (error, authData) => {
-            if (error) {
-                res.json({ estado: "error", mensaje: error })
-            } else {
-                const strFecha = fechaActual.getFullYear() + "-" + (fechaActual.getMonth() + 1) + "-" + fechaActual.getDate();
-                req.body.fecha_insert = strFecha;
-                req.body.fecha_upd = strFecha;
-                req.body.idusuario_upd = authData?.rsusuario?.idusuario;
-                const det_inventarios = await det_inventario.create(req.body, { transaction: t });
-                t.commit();
-                res.json({
-                    estado: "successfully",
-                    mensaje: "Registro almacenado",
-                    body: det_inventarios
-                })
-            }
-        })
+
+        const strFecha = fechaActual.getFullYear() + "-" + (fechaActual.getMonth() + 1) + "-" + fechaActual.getDate();
+        req.body.fecha_insert = strFecha;
+        req.body.fecha_upd = strFecha;
+        req.body.idusuario_upd = authData?.rsusuario?.idusuario;
+        await det_inventario.create(req.body, { transaction: t }).then(response => {
+            t.commit();
+            res.json({
+                mensaje: "successfully",
+                detmensaje: "Registro almacenado satisfactoriamente",
+                authData: authData,
+                body: response
+            });
+        });
     } catch (error) {
-        //console.log('Error catch', error);
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
         t.rollback();
-        res.json({ estado: "error", mensaje: error })
     }
 
 })
 
-routes.put('/put/:iddet_inventario', verificaToken, async (req, res) => {
+routes.put('/put/:iddet_inventario', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
     const t = await database.transaction();
     try {
         const det_inventarios = await det_inventario.update(req.body, { where: { iddet_inventario: req.params.iddet_inventario } }, {
             transaction: t
+        }).then(response => {
+            t.commit();
+            res.json({
+                mensaje: "successfully",
+                detmensaje: "Registro actualizado satisfactoriamente",
+                authData: authData,
+                body: response
+            });
         });
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ estado: "error", mensaje: error })
-            } else {
-                t.commit();
-                res.json({
-                    estado: "successfully",
-                    mensaje: "Registro actualizado",
-                    body: det_inventarios
-                })
-            }
-        })
     } catch (error) {
-        res.json({ estado: "error", mensaje: error })
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
         t.rollback();
     }
 })
 
-routes.put('/inactiva/:iddet_inventario', verificaToken, async (req, res) => {
+routes.put('/inactiva/:iddet_inventario', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
     const t = await database.transaction();
     try {
-        jwt.verify(req.token, process.env.CLAVESECRETA, async (error, authData) => {
-            if (error) {
-                res.json({ estado: "error", mensaje: error })
-            } else {
-                //Captura parametro
-                req.body.idusuario_upd = authData?.rsusuario?.idusuario;
-                const { cantidad, idinventario, estado } = req.body;
-                //Query de actualizacion de cabecera
-                const query = `update inventario set cantidad_total=(cantidad_total - ${cantidad})  where idinventario = ${idinventario}`;
-                await database.query(query, {
-                    transaction: t
-                });
-                //Inactivacion de detalle
-                const det_inventarios = await det_inventario.update(req.body, { where: { iddet_inventario: req.params.iddet_inventario } }, {
-                    transaction: t
-                });
-                t.commit();
-                res.json({
-                    estado: "successfully",
-                    mensaje: "Registro actualizado",
-                    body: det_inventarios
-                })
-            }
-        })
+
+        //Captura parametro
+        req.body.idusuario_upd = authData?.rsusuario?.idusuario;
+        const { cantidad, idinventario, estado } = req.body;
+        //Query de actualizacion de cabecera
+        const query = `update inventario set cantidad_total=(cantidad_total - ${cantidad})  where idinventario = ${idinventario}`;
+        await database.query(query, {
+            transaction: t
+        });
+        //Inactivacion de detalle
+        await det_inventario.update(req.body, { where: { iddet_inventario: req.params.iddet_inventario } }, {
+            transaction: t
+        }).then(response => {
+            t.commit();
+            res.json({
+                mensaje: "successfully",
+                detmensaje: "Registro actualizado satisfactoriamente",
+                authData: authData,
+                body: response
+            });
+        });
     } catch (error) {
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
         t.rollback();
-        res.json({ estado: "error", mensaje: error })
     }
 });
 
-routes.delete('/del/:iddet_inventario', verificaToken, async (req, res) => {
+routes.delete('/del/:iddet_inventario', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
     const t = await database.transaction();
     try {
-        const det_inventarios = await det_inventario.destroy({ where: { iddet_inventario: req.params.iddet_inventario } }, {
+        await det_inventario.destroy({ where: { iddet_inventario: req.params.iddet_inventario } }, {
             transaction: t
+        }).then(response => {
+            t.commit();
+            res.json({
+                mensaje: "successfully",
+                detmensaje: "Registro eliminado satisfactoriamente",
+                authData: authData,
+                body: response
+            });
         });
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ estado: "error", mensaje: error })
-            } else {
-                t.commit();
-                res.json({
-                    estado: "successfully",
-                    mensaje: "Registro eliminado",
-                    body: det_inventarios
-                })
-            }
-        })
     } catch (error) {
-        res.json({ estado: "error", mensaje: error })
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
         t.rollback();
     }
 
