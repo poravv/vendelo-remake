a8a552243bbe1e05aa030235203955a9ece3f2c0
explwi <andres.vera@tigo.net.py>
2024-08-15 20:06:25 -0400

    Refactor proveedor

--- src/servicios/proveedor.js
+++ src/servicios/proveedor.js
@@ -1,142 +1,146 @@
 const express = require('express');
 const routes = express.Router();
-const jwt = require("jsonwebtoken");
 const proveedor = require("../model/model_proveedor")
 const database = require('../database');
 const { QueryTypes } = require("sequelize")
-//const e = require('express');
-const verificaToken = require('../middleware/token_extractor')
+const { keycloak } = require('../middleware/keycloak_validate');
 require("dotenv").config()
 let fechaActual = new Date();
 
 
-routes.get('/getsql/', verificaToken, async (req, res) => {
-    try {
-        const proveedores = await database.query(`select * from proveedor where estado='AC'`, { type: QueryTypes.SELECT })
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ estado: "error", mensaje: error, })
-            } else {
-                res.json({
-                    estado: "successfully",
-                    body: proveedores
-                })
-            }
-        })
-    } catch (error) {
-        res.json({ estado: "error", mensaje: error, })
-    }
+routes.get('/getsql/', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
+    await database.query(`select * from proveedor where estado='AC'`, { type: QueryTypes.SELECT })
+        .then((response) => {
+            res.json({
+                mensaje: "successfully",
+                authData: authData,
+                body: response
+            });
+        }).catch(error => {
+            res.json({
+                mensaje: "error",
+                error: error,
+                detmensaje: `Error en el servidor, ${error}`
+            });
+        });
 })
 
-routes.get('/get/', verificaToken, async (req, res) => {
-
-    try {
-        const proveedores = await proveedor.findAll()
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ estado: "error" });
-            } else {
-                res.json({
-                    estado: "successfully",
-                    body: proveedores
-                })
-            }
-        })
-    } catch (error) {
-        res.json({ estado: "error", mensaje: error })
-    }
+routes.get('/get/', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
+    await proveedor.findAll().then((response) => {
+        res.json({
+            mensaje: "successfully",
+            authData: authData,
+            body: response
+        });
+    }).catch(error => {
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
+    });
 })
 
-routes.get('/get/:idproveedor', verificaToken, async (req, res) => {
-    try {
-        const proveedores = await proveedor.findByPk(req.params.idproveedor)
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ estado: "error", mensaje: error });
-            } else {
-                res.json({
-                    estado: "successfully",
-                    body: proveedores
-                })
-            }
-        })
-    } catch (error) {
-        res.json({ estado: "error", mensaje: error })
-    }
+routes.get('/get/:idproveedor', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
+    await proveedor.findByPk(req.params.idproveedor).then((response) => {
+        res.json({
+            mensaje: "successfully",
+            authData: authData,
+            body: response
+        });
+    }).catch(error => {
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
+    });
 })
 
-routes.post('/post/', verificaToken, async (req, res) => {
+routes.post('/post/', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
     const t = await database.transaction();
     try {
-        jwt.verify(req.token, process.env.CLAVESECRETA, async (error, authData) => {
-
-            if (error) {
-                res.json({ estado: "error", mensaje: error });
-            } else {
-                const strFecha = fechaActual.getFullYear() + "-" + (fechaActual.getMonth() + 1) + "-" + fechaActual.getDate();
-                req.body.fecha_insert = strFecha;
-                req.body.fecha_upd = strFecha;
-                req.body.idusuario_upd = authData?.rsusuario?.idusuario;
-                const proveedores = await proveedor.create(req.body, { transaction: t })
-                t.commit();
-                res.json({
-                    estado: 'successfully',
-                    mensaje: "Registro almacenado",
-                    body: proveedores
-                })
-            }
-        })
+        const strFecha = fechaActual.getFullYear() + "-" + (fechaActual.getMonth() + 1) + "-" + fechaActual.getDate();
+        req.body.fecha_insert = strFecha;
+        req.body.fecha_upd = strFecha;
+        req.body.idusuario_upd = authData?.rsusuario?.idusuario;
+        await proveedor.create(req.body, { transaction: t }).then(response => {
+            t.commit();
+            res.json({
+                mensaje: "successfully",
+                detmensaje: "Registro almacenado satisfactoriamente",
+                authData: authData,
+                body: response
+            });
+        });
     } catch (error) {
-        res.json({ estado: "error", mensaje: error })
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
         t.rollback();
     }
 
 })
 
-routes.put('/put/:idproveedor', verificaToken, async (req, res) => {
+routes.put('/put/:idproveedor', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
     const t = await database.transaction();
     try {
-        jwt.verify(req.token, process.env.CLAVESECRETA, async (error, authData) => {
-
-            if (error) {
-                res.json({ estado: "error", mensaje: error });
-            } else {
-                const strFecha = fechaActual.getFullYear() + "-" + (fechaActual.getMonth() + 1) + "-" + fechaActual.getDate();
-                req.body.fecha_upd = strFecha;
-                req.body.idusuario_upd = authData?.rsusuario?.idusuario;
-                const proveedores = await proveedor.update(req.body, { where: { idproveedor: req.params.idproveedor }, transaction: t })
+        const strFecha = fechaActual.getFullYear() + "-" + (fechaActual.getMonth() + 1) + "-" + fechaActual.getDate();
+        req.body.fecha_upd = strFecha;
+        req.body.idusuario_upd = authData?.rsusuario?.idusuario;
+        await proveedor.update(req.body, { where: { idproveedor: req.params.idproveedor }, transaction: t })
+            .then(response => {
                 t.commit();
                 res.json({
-                    estado: 'successfully',
-                    mensaje: "Registro actualizado",
-                    body: proveedores
-                })
-            }
-        })
+                    mensaje: "successfully",
+                    detmensaje: "Registro actualizado satisfactoriamente",
+                    authData: authData,
+                    body: response
+                });
+            });
     } catch (error) {
-        res.json({ estado: "error", mensaje: error })
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
         t.rollback();
     }
 })
 
-routes.delete('/del/:idproveedor', verificaToken, async (req, res) => {
+routes.delete('/del/:idproveedor', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
     const t = await database.transaction();
     try {
-        const proveedores = await proveedor.destroy({ where: { idproveedor: req.params.idproveedor }, transaction: t })
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ estado: "error", mensaje: error });
-            } else {
-                t.commit();
-                res.json({
-                    estado: 'successfully',
-                    mensaje: "Registro eliminado",
-                    body: proveedores
-                })
-            }
-        })
+        await proveedor.destroy({ where: { idproveedor: req.params.idproveedor }, transaction: t })
+        .then(response => {
+            t.commit();
+            res.json({
+                mensaje: "successfully",
+                detmensaje: "Registro eliminado satisfactoriamente",
+                authData: authData,
+                body: response
+            });
+        });
     } catch (error) {
-        res.json({ estado: "error", mensaje: error })
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
         t.rollback();
     }
 })
