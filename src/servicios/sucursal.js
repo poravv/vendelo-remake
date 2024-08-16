1f3b8510968826320e1c98e957a642f6b470ceec
explwi <andres.vera@tigo.net.py>
2024-08-15 22:21:43 -0400

    Refactor sucursal

--- src/servicios/sucursal.js
+++ src/servicios/sucursal.js
@@ -1,131 +1,143 @@
 const express = require('express');
 const routes = express.Router();
-const jwt = require("jsonwebtoken");
 const ciudad = require('../model/model_ciudad');
 const sucursal = require("../model/model_sucursal")
 const database = require('../database');
 const { QueryTypes } = require("sequelize")
-const verificaToken = require('../middleware/token_extractor')
+const { keycloak } = require('../middleware/keycloak_validate');
 require("dotenv").config()
 
-routes.get('/getsql/', verificaToken, async (req, res) => {
-    try {
-        const sucursales = await database.query('select * from vw_sucursal', { type: QueryTypes.SELECT })
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ estado: "error", mensaje: error, })
-            } else {
-                res.json({
-                    estado: "successfully",
-                    body: sucursales
-                })
-            }
-        })
-    } catch (error) {
-        res.json({ estado: "error", mensaje: error, })
-    }
+routes.get('/getsql/', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
+    await database.query('select * from vw_sucursal', { type: QueryTypes.SELECT })
+        .then((response) => {
+            res.json({
+                mensaje: "successfully",
+                authData: authData,
+                body: response
+            });
+        }).catch(error => {
+            res.json({
+                mensaje: "error",
+                error: error,
+                detmensaje: `Error en el servidor, ${error}`
+            });
+        });
 })
 
 
-routes.get('/get/', verificaToken, async (req, res) => {
-    try {
-        const sucursales = await sucursal.findAll({ include: ciudad })
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ error: "Error ", error });
-            } else {
-                res.json({
-                    estado: "successfully",
-                    body: sucursales
-                })
-            }
-        })
-    } catch (error) {
-        res.json({ estado: "error", mensaje: error, })
-    }
+routes.get('/get/', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
+
+    await sucursal.findAll({ include: ciudad })
+        .then((response) => {
+            res.json({
+                mensaje: "successfully",
+                authData: authData,
+                body: response
+            });
+        }).catch(error => {
+            res.json({
+                mensaje: "error",
+                error: error,
+                detmensaje: `Error en el servidor, ${error}`
+            });
+        });
 })
 
-routes.get('/get/:idsucursal', verificaToken, async (req, res) => {
-    try {
-        const sucursales = await sucursal.findByPk(req.params.idsucursal, { include: ciudad })
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ error: "Error ", error });
-            } else {
-                res.json({
-                    estado:"successfully",
-                    body: sucursales
-                })
-            }
-        })
-    } catch (error) {
-        res.json({ estado: "error", mensaje: error, })
-    }
+routes.get('/get/:idsucursal', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
+    await sucursal.findByPk(req.params.idsucursal, { include: ciudad })
+        .then((response) => {
+            res.json({
+                mensaje: "successfully",
+                authData: authData,
+                body: response
+            });
+        }).catch(error => {
+            res.json({
+                mensaje: "error",
+                error: error,
+                detmensaje: `Error en el servidor, ${error}`
+            });
+        });
 })
 
-routes.post('/post/', verificaToken, async (req, res) => {
+routes.post('/post/', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
     const t = await database.transaction();
     try {
-        const sucursales = await sucursal.create(req.body, { transaction: t })
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ error: "Error ", error });
-            } else {
+        await sucursal.create(req.body, { transaction: t })
+            .then(response => {
                 t.commit();
                 res.json({
-                    estado:"successfully",
-                    mensaje: "Registro almacenado correctamente",
-                    body: sucursales
-                })
-            }
-        })
+                    mensaje: "successfully",
+                    detmensaje: "Registro almacenado satisfactoriamente",
+                    authData: authData,
+                    body: response
+                });
+            });
     } catch (error) {
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
         t.rollback();
-        res.json({ estado: "error", mensaje: error, })
     }
 })
 
-routes.put('/put/:idsucursal', verificaToken, async (req, res) => {
+routes.put('/put/:idsucursal', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
     const t = await database.transaction();
     try {
-        const sucursales = await sucursal.update(req.body, { where: { idsucursal: req.params.idsucursal }, transaction: t })
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ error: "Error ", error });
-            } else {
+        await sucursal.update(req.body, { where: { idsucursal: req.params.idsucursal }, transaction: t })
+            .then(response => {
                 t.commit();
                 res.json({
-                    estado:"successfully",
-                    mensaje: "Registro actualizado correctamente",
-                    body: sucursales
-                })
-            }
-        })
+                    mensaje: "successfully",
+                    detmensaje: "Registro actualizado satisfactoriamente",
+                    authData: authData,
+                    body: response
+                });
+            });
     } catch (error) {
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
         t.rollback();
-        res.json({ estado: "error", mensaje: error, })
     }
 })
 
-routes.delete('/del/:idsucursal', verificaToken, async (req, res) => {
+routes.delete('/del/:idsucursal', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
     const t = await database.transaction();
     try {
-        const sucursales = await sucursal.destroy({ where: { idsucursal: req.params.idsucursal }, transaction: t })
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ error: "Error ", error });
-            } else {
+        await sucursal.destroy({ where: { idsucursal: req.params.idsucursal }, transaction: t })
+            .then(response => {
                 t.commit();
                 res.json({
-                    estado:"successfully",
-                    mensaje: "Registro eliminado",
-                    body: sucursales
-                })
-            }
-        })
+                    mensaje: "successfully",
+                    detmensaje: "Registro eliminado satisfactoriamente",
+                    authData: authData,
+                    body: response
+                });
+            });
     } catch (error) {
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
         t.rollback();
-        res.json({ estado: "error", mensaje: error, })
     }
 })
 
