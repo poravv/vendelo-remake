4fd6d2624d07af1c940cb8089fe781181d209c17
explwi <andres.vera@tigo.net.py>
2024-08-15 22:14:21 -0400

    Refactor receta

--- src/servicios/receta.js
+++ src/servicios/receta.js
@@ -1,109 +1,125 @@
 const express = require('express');
 const routes = express.Router();
-const jwt = require("jsonwebtoken");
 const receta = require("../model/model_receta")
 const producto_final = require("../model/model_producto_final")
 const database = require('../database');
-const e = require('express');
-const verificaToken = require('../middleware/token_extractor')
+const { keycloak } = require('../middleware/keycloak_validate');
 require("dotenv").config()
 
-routes.get('/get/', verificaToken, async (req, res) => {
-    const recetas = await receta.findAll({ include: producto_final })
-    jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-        if (error) {
-            res.json({ estado: "error", mensaje: error })
-        } else {
+routes.get('/get/', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
+    await receta.findAll({ include: producto_final })
+        .then((response) => {
             res.json({
-                estado: "successfully",
-                body: recetas
-            })
-        }
-    })
-})
-
-routes.get('/get/:idreceta', verificaToken, async (req, res) => {
-    const recetas = await receta.findByPk(req.params.idreceta, { include: producto_final })
-    jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-        if (error) {
-            res.json({ estado: "error", mensaje: error })
-        } else {
+                mensaje: "successfully",
+                authData: authData,
+                body: response
+            });
+        }).catch(error => {
             res.json({
-                estado: "successfully",
-                body: recetas
-            })
-        }
-    })
+                mensaje: "error",
+                error: error,
+                detmensaje: `Error en el servidor, ${error}`
+            });
+        });
 })
 
-routes.post('/post/', verificaToken, async (req, res) => {
+routes.get('/get/:idreceta', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
+    await receta.findByPk(req.params.idreceta, { include: producto_final })
+        .then((response) => {
+            res.json({
+                mensaje: "successfully",
+                authData: authData,
+                body: response
+            });
+        }).catch(error => {
+            res.json({
+                mensaje: "error",
+                error: error,
+                detmensaje: `Error en el servidor, ${error}`
+            });
+        });
+})
+
+routes.post('/post/', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
     const t = await database.transaction();
-    console.log('Entra en receta-----------------------------------------------')
+    //console.log('Entra en receta-----------------------------------------------')
     try {
-        const recetas = await receta.create(req.body, { transaction: t })
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ estado: "error", mensaje: error })
-            } else {
+        await receta.create(req.body, { transaction: t })
+            .then(response => {
                 t.commit();
                 res.json({
-                    estado: "successfully",
-                    mensaje: "Registro almacenado correctamente",
-                    body: recetas
-                })
-            }
-        })
+                    mensaje: "successfully",
+                    detmensaje: "Registro almacenado satisfactoriamente",
+                    authData: authData,
+                    body: response
+                });
+            });
     } catch (error) {
-        res.json({ estado: "error", mensaje: error })
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
         t.rollback();
     }
 
 })
 
-routes.put('/put/:idreceta', verificaToken, async (req, res) => {
+routes.put('/put/:idreceta', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
     const t = await database.transaction();
     try {
-        const recetas = await receta.update(req.body, { where: { idreceta: req.params.idreceta }, transaction: t })
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ estado: "error", mensaje: error })
-            } else {
+        await receta.update(req.body, { where: { idreceta: req.params.idreceta }, transaction: t })
+            .then(response => {
                 t.commit();
                 res.json({
-                    estado: "successfully",
-                    mensaje: "Registro actualizado correctamente",
-                    body: recetas
-                })
-            }
-        })
+                    mensaje: "successfully",
+                    detmensaje: "Registro actualizado satisfactoriamente",
+                    authData: authData,
+                    body: response
+                });
+            });
     } catch (error) {
-        res.json({ estado: "error", mensaje: error })
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
         t.rollback();
     }
 
 })
 
-routes.delete('/del/:idreceta', verificaToken, async (req, res) => {
+routes.delete('/del/:idreceta', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
     const t = await database.transaction();
     try {
-        const recetas = await receta.destroy({ where: { idreceta: req.params.idreceta }, transaction: t })
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ estado: "error", mensaje: error })
-            } else {
+        await receta.destroy({ where: { idreceta: req.params.idreceta }, transaction: t })
+            .then(response => {
                 t.commit();
                 res.json({
-                    estado: "successfully",
-                    mensaje: "Registro eliminado",
-                    body: recetas
-                })
-            }
-        })
+                    mensaje: "successfully",
+                    detmensaje: "Registro eliminado satisfactoriamente",
+                    authData: authData,
+                    body: response
+                });
+            });
     } catch (error) {
-        res.json({ estado: "error", mensaje: error })
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
         t.rollback();
     }
-
 })
 
 
