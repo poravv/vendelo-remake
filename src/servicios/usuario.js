3d81c36ef55b3d85265a9ecb67c40ff4a23b258a
explwi <andres.vera@tigo.net.py>
2024-08-15 22:29:42 -0400

    Refactor usuario

--- src/servicios/usuario.js
+++ src/servicios/usuario.js
@@ -5,7 +5,7 @@ const usuario = require("../model/model_usuario")
 const sucursal = require("../model/model_sucursal")
 const persona = require("../model/model_persona")
 const database = require('../database')
-const verificaToken = require('../middleware/token_extractor')
+const { keycloak } = require('../middleware/keycloak_validate');
 const md5 = require('md5')
 require("dotenv").config()
 
@@ -25,18 +25,13 @@ routes.post('/login/', async (req, res) => {
         await database.query('CALL cargaInventarioCab(@a)');
 
         if (rsusuario.length != 0) {
-
             //console.log('Mi usuario: ', rsusuario)
-
             jwt.sign({ rsusuario }, process.env.CLAVESECRETA
                 , { expiresIn: '12h' }//Para personalizar el tiempo para expirar
                 , (error, token) => {
-                    
-                    console.log("-------------->",token);
-
+                    //console.log("-------------->",token);
                     res.json({ estado: "succesfully", token, body: rsusuario })
                 });
-
         } else {
             return res.status(400).json(
                 {
@@ -55,140 +50,153 @@ routes.post('/login/', async (req, res) => {
     }
 })
 
-routes.get('/get/', verificaToken, async (req, res) => {
-    const usuarios = await usuario.findAll({
+routes.get('/get/', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
+    await usuario.findAll({
         include: [
             { model: sucursal },
             { model: persona }
         ]
-    })
-
-    jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-        if (error) {
-            res.json({ estado: "error", mensaje: error });
-        } else {
-            res.json({
-                estado: "successfully",
-                body: usuarios
-            })
-        }
-    })
+    }).then((response) => {
+        res.json({
+            mensaje: "successfully",
+            authData: authData,
+            body: response
+        });
+    }).catch(error => {
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
+    });
 })
 
-routes.get('/get/:idusuario', verificaToken, async (req, res) => {
-    const usuarios = await usuario.findByPk(req.params.idusuario, {
+routes.get('/get/:idusuario', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
+    await usuario.findByPk(req.params.idusuario, {
         include: [
             { model: sucursal },
             { model: persona }
         ]
-    })
-    jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-        if (error) {
-            res.json({ estado: "error", mensaje: error });
-        } else {
-            res.json({
-                estado: "successfully",
-                body: usuarios
-            })
-        }
-    })
+    }).then((response) => {
+        res.json({
+            mensaje: "successfully",
+            authData: authData,
+            body: response
+        });
+    }).catch(error => {
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
+    });
 })
 
 
-routes.put('/reset/:idusuario', verificaToken, async (req, res) => {
+routes.put('/reset/:idusuario', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
     const t = await database.transaction();
     //console.log(req.body)
     req.body.password = md5(req.body.password);
     try {
-        await usuario.update(req.body, { where: { idusuario: req.params.idusuario }, transaction: t }).then((rs) => {
-            //console.log('RS: ', rs)
-            jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-                if (error) {
-                    res.json({ estado: "error", mensaje: error });
-                } else {
-                    t.commit();
-                    res.json({
-                        estado: "successfully",
-                        mensaje: "Usuario actualizado correctamente",
-                        body: rs
-                    })
-                }
-            })
+        await usuario.update(req.body, { where: { idusuario: req.params.idusuario }, transaction: t })
+            .then(response => {
+                t.commit();
+                res.json({
+                    mensaje: "successfully",
+                    detmensaje: "Registro actualizado satisfactoriamente",
+                    authData: authData,
+                    body: response
+                });
+            });
+    } catch (error) {
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
         });
-
-    } catch (error) {
-        res.json({ estado: "error", mensaje: error });
         t.rollback();
     }
 })
 
-routes.post('/post/', verificaToken, async (req, res) => {
+routes.post('/post/', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
     const t = await database.transaction();
-
     try {
-
-        jwt.verify(req.token, process.env.CLAVESECRETA, async (error, authData) => {
-            if (error) {
-                res.json({ estado: "error", mensaje: error });
-            } else {
-                req.body.password = md5(req.body.password);
-                const usuarios = await usuario.create(req.body, { transaction: t })
+        req.body.password = md5(req.body.password);
+        const usuarios = await usuario.create(req.body, { transaction: t })
+            .then(response => {
                 t.commit();
                 res.json({
-                    estado: "successfully",
-                    mensaje: "Registro almacenado",
-                    body: usuarios
-                })
-            }
-        })
+                    mensaje: "successfully",
+                    detmensaje: "Registro almacenado satisfactoriamente",
+                    authData: authData,
+                    body: response
+                });
+            });
     } catch (error) {
-        res.json({ estado: "error", mensaje: error });
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
+        t.rollback();
+    }
+})
+
+routes.put('/put/:idusuario', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
+    const t = await database.transaction();
+    try {
+        await usuario.update(req.body, { where: { idusuario: req.params.idusuario }, transaction: t })
+            .then(response => {
+                t.commit();
+                res.json({
+                    mensaje: "successfully",
+                    detmensaje: "Registro actualizado satisfactoriamente",
+                    authData: authData,
+                    body: response
+                });
+            });
+    } catch (error) {
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
         t.rollback();
     }
 
 })
 
-routes.put('/put/:idusuario', verificaToken, async (req, res) => {
+routes.delete('/del/:idusuario', keycloak.protect(), async (req, res) => {
+    const token = req.kauth.grant.access_token;
+    const authData = token.content;
     const t = await database.transaction();
     try {
-        const usuarios = await usuario.update(req.body, { where: { idusuario: req.params.idusuario }, transaction: t })
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ estado: "error", mensaje: error });
-            } else {
+        await usuario.destroy({ where: { idusuario: req.params.idusuario }, transaction: t })
+            .then(response => {
                 t.commit();
                 res.json({
-                    estado: "successfully",
-                    mensaje: "Registro actualizado",
-                    body: usuarios
-                })
-            }
-        })
+                    mensaje: "successfully",
+                    detmensaje: "Registro eliminado satisfactoriamente",
+                    authData: authData,
+                    body: response
+                });
+            });
     } catch (error) {
-        res.json({ estado: "error", mensaje: error });
-        t.rollback();
-    }
-
-})
-
-routes.delete('/del/:idusuario', verificaToken, async (req, res) => {
-    const t = await database.transaction();
-    try {
-        const usuarios = await usuario.destroy({ where: { idusuario: req.params.idusuario }, transaction: t })
-        jwt.verify(req.token, process.env.CLAVESECRETA, (error, authData) => {
-            if (error) {
-                res.json({ estado: "error", mensaje: error });
-            } else {
-                t.commit();
-                res.json({
-                    estado: "successfully",
-                    mensaje: "Registro eliminado",
-                    body: usuarios
-                })
-            }
-        })
-    } catch (error) {
-        res.json({ estado: "error", mensaje: error });
+        res.json({
+            mensaje: "error",
+            error: error,
+            detmensaje: `Error en el servidor, ${error}`
+        });
         t.rollback();
     }
 })
